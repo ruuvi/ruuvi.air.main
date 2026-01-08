55f607c90229188cde0db47aadd5d169fa084ff4
TheSomeMan <alexeyn.skvortsov@gmail.com>
2026-01-08 20:22:31 +0700

    [#11] Fix build in Docker

--- build_nrf52840dk_a2_debug_mock.sh
+++ build_nrf52840dk_a2_debug_mock.sh
@@ -8,5 +8,5 @@ exec ./build.sh \
   --board_rev_name=RuuviAir-A2 \
   --build_mode=debug \
   --build_dir_suffix=mock \
-  --extra_cflags="-DRUUVI_MOCK_MEASUREMENTS=1"
+  --extra_cflags="-DRUUVI_MOCK_MEASUREMENTS=1" \
   "$@"
